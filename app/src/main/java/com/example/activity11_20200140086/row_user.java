6a24708c265e64e65138c2f22d3ec258cc55fc9e
alamnurcahaya <lammnurc@outlook.com>
2022-06-11 12:42:00 +0700

    V1.0 Activity 11: Firebase with Firebase Assistance

---
+++ app/src/main/java/com/example/activity11_20200140086/row_user.java
@@ -0,0 +1,14 @@
+package com.example.activity11_20200140086;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class row_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_row_user);
+    }
+}
